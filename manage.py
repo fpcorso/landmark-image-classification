a338890bd9f02a0a29ddbc701775fa574351c27b
Frank Corso <frank@frankcorso.me>
2023-06-17 11:07:57 -0400

    Update setup script to use new methods

--- manage.py
+++ manage.py
@@ -1,60 +1,22 @@
-from io import BytesIO
-import os
-import torch
-import torch.utils.data
 import typer
-import urllib.request
-from zipfile import ZipFile
-from torchvision import datasets, transforms
-import multiprocessing
+from src.data import cache_data_mean_and_std, download_and_extract_data
 
 app = typer.Typer()
 
 
 @app.command()
 def setup():
-    typer.echo("Initializing database...")
+    typer.echo("Initializing project...")
 
-    data_folder = "landmark_images"
+    download_and_extract_data()
+    cache_data_mean_and_std()
 
-    if not os.path.exists(data_folder):
-        typer.echo("Downloading dataset...")
-        url = "https://udacity-dlnfd.s3-us-west-1.amazonaws.com/datasets/landmark_images.zip"
-        with urllib.request.urlopen(url) as resp:
-            with ZipFile(BytesIO(resp.read())) as fp:
-                fp.extractall(".")
+    typer.echo("Initialization complete")
 
-        typer.echo("Download complete.")
 
-    cache_file = "mean_and_std.pt"
-
-    if not os.path.exists(cache_file):
-        ds = datasets.ImageFolder(
-            data_folder, transform=transforms.Compose([transforms.ToTensor()])
-        )
-        dl = torch.utils.data.DataLoader(
-            ds, batch_size=1, num_workers=multiprocessing.cpu_count()
-        )
-
-        mean = 0.0
-        for images, _ in dl:
-            batch_samples = images.size(0)
-            images = images.view(batch_samples, images.size(1), -1)
-            mean += images.mean(2).sum(0)
-        mean = mean / len(dl.dataset)
-
-        var = 0.0
-        npix = 0
-        for images, _ in dl:
-            batch_samples = images.size(0)
-            images = images.view(batch_samples, images.size(1), -1)
-            var += ((images - mean.unsqueeze(1)) ** 2).sum([0, 2])
-            npix += images.nelement()
-
-        std = torch.sqrt(var / (npix / 3))
-
-        # Cache results so we don't need to redo the computation
-        torch.save({"mean": mean, "std": std}, cache_file)
+@app.command()
+def hello():
+    typer.echo("Hello!")
 
 
 if __name__ == "__main__":
