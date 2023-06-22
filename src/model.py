f7991e10aa3a4491106852ee1ac719b0c54cb6ae
Frank Corso <frank@frankcorso.me>
2023-06-22 15:22:54 -0400

    Create initial CNN

---
+++ src/model.py
@@ -0,0 +1,62 @@
+import torch
+import torch.nn as nn
+
+
+# define the CNN architecture
+class MyModel(nn.Module):
+    def __init__(self, num_classes: int = 1000, dropout: float = 0.7) -> None:
+        super().__init__()
+
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 32, 3, padding=1),
+            nn.MaxPool2d(2, 2),
+            nn.ReLU(),
+
+            nn.Conv2d(32, 64, 3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.MaxPool2d(2, 2),
+            nn.ReLU(),
+            nn.Dropout2d(dropout),
+
+            nn.Conv2d(64, 128, 3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.MaxPool2d(2, 2),
+            nn.ReLU(),
+            nn.Dropout2d(dropout),
+
+            nn.Conv2d(128, 256, 3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.MaxPool2d(2, 2),
+            nn.ReLU(),
+            nn.Dropout2d(dropout),
+
+            nn.Conv2d(256, 512, 3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.MaxPool2d(2, 2),
+            nn.ReLU(),
+            nn.Dropout2d(dropout),
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 5120),
+            nn.BatchNorm1d(5120),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+
+            nn.Linear(5120, 1024),
+            nn.BatchNorm1d(1024),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+
+            nn.Linear(1024, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+
+            nn.Linear(512, num_classes),
+        )
+
+        self.model = nn.Sequential(self.features, nn.Flatten(), self.classifier)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.model(x)
