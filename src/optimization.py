01f6042a421522fe49052ded5d0e23efe2b26c6e
Frank Corso <frank@frankcorso.me>
2023-06-23 11:41:14 -0400

    Create functions for getting loss and optimizer

---
+++ src/optimization.py
@@ -0,0 +1,49 @@
+import torch
+import torch.nn as nn
+import torch.optim
+import pytest
+
+
+def get_loss():
+    """
+    Get an instance of the CrossEntropyLoss (useful for classification),
+    optionally moving it to the GPU if use_cuda is set to True
+    """
+
+    loss = nn.CrossEntropyLoss()
+
+    return loss
+
+
+def get_optimizer(
+    model: nn.Module,
+    optimizer: str = "SGD",
+    learning_rate: float = 0.01,
+    momentum: float = 0.5,
+    weight_decay: float = 0,
+):
+    """
+    Returns an optimizer instance
+
+    :param model: the model to optimize
+    :param optimizer: one of 'SGD' or 'Adam'
+    :param learning_rate: the learning rate
+    :param momentum: the momentum (if the optimizer uses it)
+    :param weight_decay: regularization coefficient
+    """
+    if optimizer.lower() == "sgd":
+        opt = torch.optim.SGD(
+            model.parameters(),
+            lr=learning_rate,
+            momentum=momentum,
+            weight_decay=weight_decay,
+        )
+
+    elif optimizer.lower() == "adam":
+        opt = torch.optim.Adam(
+            model.parameters(), lr=learning_rate, weight_decay=weight_decay
+        )
+    else:
+        raise ValueError(f"Optimizer {optimizer} not supported")
+
+    return opt
