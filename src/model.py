fa9b9b6e422423209222294112d7ae5c91c86868
Frank Corso <frank@frankcorso.me>
2023-07-08 15:40:17 -0400

    Create function for creating model using transfer learning

--- src/model.py
+++ src/model.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+import torchvision
+import torchvision.models as models
 
 
 # define the CNN architecture
@@ -53,3 +55,30 @@ class MyModel(nn.Module):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.model(x)
+
+
+def get_transfer_learning_model(model_name="resnet18", n_classes=50):
+    """Creates a model using an existing model as the foundation and
+    replacing the linear layer to meet the needs for this model."""
+
+    # Get the requested architecture
+    if hasattr(models, model_name):
+        model_transfer = getattr(models, model_name)(weights="DEFAULT")
+
+    else:
+        torchvision_major_minor = ".".join(torchvision.__version__.split(".")[:2])
+
+        raise ValueError(
+            f"Model {model_name} is not known. List of available models: "
+            f"https://pytorch.org/vision/{torchvision_major_minor}/models.html"
+        )
+
+    # Freeze all parameters in the model
+    for p in model_transfer.parameters():
+        p.freeze = True
+
+    # Add the linear layer at the end with the appropriate number of classes
+    num_ftrs = model_transfer.fc.in_features
+    model_transfer.fc = nn.Sequential(nn.Linear(num_ftrs, n_classes))
+
+    return model_transfer
